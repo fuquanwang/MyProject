88a9c84f58df0b12be4af7824c7f73e12ed6b22f
Fuquan Wang <fuquan.wang@gmail.com>
2015-06-20 02:24:00 -0700

    Add test cases

--- src/main/java/com/fuquan/utils/Utils.java
+++ src/main/java/com/fuquan/utils/Utils.java
@@ -1,7 +1,9 @@
 package com.fuquan.utils;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fuquan.pojo.People;
+import com.google.common.base.Throwables;
 
 /**
  * Created by Fuquan on 6/20/2015.
@@ -11,11 +13,15 @@ public class Utils {
     static ObjectMapper ob = new ObjectMapper();
 
     public static String peopleToJSON (People people) throws Exception {
-
         return ob.writerWithDefaultPrettyPrinter().writeValueAsString(people);
     }
 
-    public static People JSONToPeople(String json) throws Exception {
-        return ob.readValue(json, People.class);
+    public static People JSONToPeople(String json) {
+        try {
+            return ob.readValue(json, People.class);
+        } catch(Exception ex) {
+            Throwables.getStackTraceAsString(ex);
+        }
+        return null;
     }
 }
---
+++ src/test/java/com/fuquan/utils/UtilsTest.java
@@ -0,0 +1,19 @@
+package com.fuquan.utils;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fuquan.pojo.People;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNull;
+
+/**
+ * Created by Fuquan on 6/20/2015.
+ */
+public class UtilsTest {
+    @Test
+    public void testExpectException() {
+        String json = "{\"test\":4}";
+        People p = Utils.JSONToPeople(json);
+        assertNull(p);
+    }
+}
