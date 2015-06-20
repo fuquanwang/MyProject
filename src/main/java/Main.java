a11d5ca6b8aceaffe4549c3cebe9733f7e8ab409
Fuquan Wang <fuquan.wang@gmail.com>
2015-06-20 01:14:28 -0700

    New files

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Fuquan on 6/20/2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Something");
+    }
+}
---
+++ src/main/java/Utils.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Fuquan on 6/20/2015.
+ */
+public class Utils {
+}
